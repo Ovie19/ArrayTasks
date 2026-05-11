a15c8f7154fa723751541b24e575c12286ea5fa5
Ovie19 <adetolabenjamin18@gmail.com>
2026-05-11 16:08:01 +0100

    A function and its test to return an array with only the even numbers

--- FireDrill/TaskFourTest.java
+++ FireDrill/TaskFourTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class TaskFourTest {
     @Test
-    public void testThatTaskFourReturnsAnArrayWithAllEvenNumbersRemoved() {
+    public void testThatTaskFourReturnsAnArrayWithAllOddNumbersRemoved() {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7};
         int[] expected = TaskFour.taskFour(numbers);
         int[] actual = {2, 4, 6};

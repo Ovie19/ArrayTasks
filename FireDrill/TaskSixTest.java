ed6a1743cde09d1d807d2a9dc9300544d80658a4
Ovie19 <adetolabenjamin18@gmail.com>
2026-05-11 16:44:35 +0100

    A function and its test to return an array in ascending order

---
+++ FireDrill/TaskSixTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class TaskSixTest {
+    @Test
+    public void testThatTaskSixReturnsAnArrayWithDuplicatesNumbersRemoved() {
+        int[] numbers = {12, 19, 13, 11, 4, 9};
+        int[] expected = TaskSix.taskSix(numbers);
+        int[] actual = {4, 9, 11, 12, 13, 19};
+        assertArrayEquals(actual, expected);
+    }
+}
